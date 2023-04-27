3281357ee0b4a4189051751ea8a2e2fdbe171a92
krs131099 <krs.nandhan999@gmail.com>
2023-04-27 12:55:51 -0500

    Completed delete() in SerialBST.java

--- src/edu/iastate/cs527/impl/SerialBST.java
+++ src/edu/iastate/cs527/impl/SerialBST.java
@@ -10,13 +10,14 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class SerialBST<T extends Number> implements BST<T> {
 
+    // TODO complete delete function.
+
     // create root node
     Node<T> root;
-    int count;
 
     public SerialBST(T root) {
         this.root = new Node<>(root);
-        this.count++;
+
     }
 
     @Override
@@ -25,10 +26,10 @@ public class SerialBST<T extends Number> implements BST<T> {
 
         while (current != null) {
             T nodeKey = current.key;
-            if (Objects.equals(nodeKey, key)) // todo check this.
+            if (equals(nodeKey, key))
                 return true;
             else {
-                if (compare(key, nodeKey)) { // todo create new comparator
+                if (compare(key, nodeKey)) {
                     current = current.left;
                 }
                 else
@@ -63,29 +64,76 @@ public class SerialBST<T extends Number> implements BST<T> {
                 current = current.right;
             }
         }
-        this.count++;
+
         return true;
     }
 
+    private boolean isLeaf(Node<T> node) {
+        return node.left == null && node.right == null;
+    }
+
+    private Node<T> getMinimum(Node<T> node) {
+        // pass right chiild.
+        while( node.left != null)
+            node = node.left;
+        return node;
+    }
     @Override
     public boolean delete(T key) {
 
-        if (!search(key))
-            return false;
+        Node<T> parent = null;
         var current = root;
-        while (current != null){
 
-            if (key.longValue() < current.key.longValue()){
-                //
-            }
+        while (current != null && !equals(current.key, key)){
+            parent = current;
+
+            if (compare(key, current.key))
+                current = current.left;
+            else current = current.right;
         }
-        this.count--;
+
+        if (current == null)
+            return false;
+
+        if (current.left == null && current.right == null){
+            // leaf node, just remove it.
+            if (current != root){
+                if (parent.left == current)
+                    parent.left = null;
+                else parent.right = null;
+            }
+            else this.root = null;
+        }
+        else if (current.left != null && current.right != null){
+            Node<T> successor = getMinimum(current.right);
+            T min_key = successor.key;
+
+            // delete successor node
+            delete(min_key);
+            current.key = min_key;
+        }
+        else {
+            // node has only one child.
+            Node<T> child = current.left != null ? current.left: current.right;
+
+            if (current != root){
+                if (current == parent.left)
+                    parent.left = child;
+                else parent.right = child;
+            }
+            else root = child;
+        }
+
         return true;
     }
 
     private boolean compare(T key1, T key2){
         return key1.intValue() < key2.intValue();
         //return false; // TODO ADD CODE.
+    }
+
+    private boolean equals(T key1, T key2) {
+        return key1.intValue() == key2.intValue();
     }
 
 
@@ -113,12 +161,21 @@ public class SerialBST<T extends Number> implements BST<T> {
         int max = 1000;
         int N = 30;
 
+        int[] vals = new int[N];
+
         for (int i = 0; i < N; i++) {
             var x = ThreadLocalRandom.current().nextInt(min, max+1);
+            vals[i] = x;
             bst.insert(x);
         }
 
+        for (int i = 0; i < 11; i++){
+            bst.delete(vals[i]);
+        }
+
+
         List<Integer> elements = bst.traverse();
+        System.out.println("Number of elemets present: " + elements.size());
         for (Integer n: elements){
             System.out.print(n+" ");
         }
@@ -134,12 +191,5 @@ class Node<T extends Number> {
         this.key = key;
         this.left = null;
         this.right = null;
-    }
-}
-
-class NumberComparator<T extends Comparable<T>> implements Comparator<T> {
-    @Override
-    public int compare(T o1, T o2) {
-        return o1.compareTo(o2);
     }
 }
