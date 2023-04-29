6833412ff6bf6004eec8d583c0c531bb27c646be
krs131099 <krs.nandhan999@gmail.com>
2023-04-29 01:26:13 -0500

    Added delete() and fixed bug in compare() in LockBST.java.

--- src/main/java/edu/iastate/cs527/impl/LockBST.java
+++ src/main/java/edu/iastate/cs527/impl/LockBST.java
@@ -1,10 +1,16 @@
 package edu.iastate.cs527.impl;
 
 import edu.iastate.cs527.BST;
+import edu.iastate.cs527.BSTThreadPool;
 import net.jcip.annotations.ThreadSafe;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
 // @ThreadSafe
 public class LockBST<T extends Number> implements BST<T> {
 
@@ -15,7 +21,8 @@ public class LockBST<T extends Number> implements BST<T> {
     }
 
     @Override
-    public boolean search(T key) {
+    public synchronized boolean search(T key) {
+        // allow concurrent search by threads? problem?
 
         var current = root;
 
@@ -32,7 +39,7 @@ public class LockBST<T extends Number> implements BST<T> {
     }
 
     @Override
-    public boolean insert(T key) {
+    public synchronized boolean insert(T key) {
         if (search(key))
             return false;
 
@@ -59,12 +66,67 @@ public class LockBST<T extends Number> implements BST<T> {
         return false;
     }
 
-    @Override
-    public boolean delete(T key) {
-        return false;
+    private boolean equals(T key1, T key2) {
+        return key1.intValue() == key2.intValue();
     }
 
-    public List<T> traverse() {
+    @Override
+    public synchronized boolean delete(T key) {
+
+        Node<T> parent = null;
+        var current = root;
+
+        while (current != null && !equals(current.key, key)){
+            parent = current;
+
+            if (compare(key, current.key))
+                current = current.left;
+            else current = current.right;
+        }
+
+        if (current == null)
+            return false;
+
+        if (current.left == null && current.right == null){
+            // leaf node, just remove it.
+            if (current != root){
+                if (parent.left == current)
+                    parent.left = null;
+                else parent.right = null;
+            }
+            else this.root = null;
+        }
+        else if (current.left != null && current.right != null){
+            Node<T> successor = getMinimum(current.right);
+            T min_key = successor.key;
+
+            // delete successor node
+            delete(min_key);
+            current.key = min_key;
+        }
+        else {
+            // node has only one child.
+            Node<T> child = current.left != null ? current.left: current.right;
+
+            if (current != root){
+                if (current == parent.left)
+                    parent.left = child;
+                else parent.right = child;
+            }
+            else root = child;
+        }
+
+        return true;
+    }
+
+    private Node<T> getMinimum(Node<T> node) {
+        // pass right chiild.
+        while( node.left != null)
+            node = node.left;
+        return node;
+    }
+
+    public synchronized List<T> traverse() {
         List<T> elements = new ArrayList<>();
         Node<T> current = this.root;
         inOrderTraversal(current, elements);
@@ -82,7 +144,25 @@ public class LockBST<T extends Number> implements BST<T> {
     }
 
     private boolean compare(T key1, T key2) {
-        return key1.intValue() < key1.intValue();
+        return key1.intValue() < key2.intValue();
+    }
+
+    public static void main(String[] args) {
+        LockBST<Integer> lockBST = new LockBST<>(500);
+
+        var numbers = ThreadLocalRandom.current().ints(100, 1,1000);
+
+        BSTThreadPool threadPool = new BSTThreadPool(3,3,1000, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+
+        numbers.forEach((i) -> threadPool.execute(()->lockBST.insert(i)) );
+
+        threadPool.shutdown();
+
+        while(!threadPool.isTerminated());
+
+        System.out.println("Printing array");
+        lockBST.traverse().forEach((i) -> System.out.print(i+" "));
+
     }
 
 
